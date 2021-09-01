e6b37a8a914462f36cb879b2cfe91e6c57e710a5
Michael Kennedy <merpso@gmail.com>
2021-09-01 13:47:23 +1000

    validated big-iq shared module

--- AWS/modules/shared/main.tf
+++ AWS/modules/shared/main.tf
@@ -19,13 +19,72 @@ resource "aws_secretsmanager_secret_version" "bigiq-pwd" {
   secret_id     = aws_secretsmanager_secret.bigiq.id
   secret_string = random_password.password.result
 }
+#
+# Create a security group for BIG-IQ Management
+#
+module "bigiq_mgmt_sg" {
+  source = "terraform-aws-modules/security-group/aws"
 
-module "big_iq_byol" {
+  name        = format("%s-mgmt-sg-%s", var.tags.prefix, random_id.id.hex)
+  description = "Security group for BIG-IQ Demo"
+  vpc_id      = var.vpc_id
+
+  ingress_cidr_blocks = [var.allowed_mgmt_cidr]
+  ingress_rules       = ["https-443-tcp", "ssh-tcp"]
+
+  ingress_with_source_security_group_id = [
+    {
+      rule                     = "all-all"
+      source_security_group_id = module.bigiq_mgmt_sg.security_group_id
+    }
+  ]
+
+  # Allow ec2 instances outbound Internet connectivity
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+}
+#
+# Create a security group for BIG-IQ
+#
+module "bigiq_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = format("%s-sg-%s", var.tags.prefix, random_id.id.hex)
+  description = "Security group for BIG-IQ Demo"
+  vpc_id      = var.vpc_id
+
+  ingress_cidr_blocks = [var.allowed_mgmt_cidr]
+  ingress_rules       = ["https-443-tcp"]
+
+  ingress_with_source_security_group_id = [
+    {
+      rule                     = "all-all"
+      source_security_group_id = module.bigiq_mgmt_sg.security_group_id
+    }
+  ]
+
+  # Allow ec2 instances outbound Internet connectivity
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+}
+
+module "bigiq" {
   source = "github.com/merps/terraform-aws-bigiq"
+
+  admin_password = random_password.password.result
+  ec2_key_name = var.ec2_key_name
+  dcd_license_keys = var.dcd_license_keys
+  cm_license_keys = var.cm_license_keys
+
   aws_secretmanager_secret_id = aws_secretsmanager_secret.bigiq.id
-  cm_license_keys = [ var.licenses.cm_key ]
-  ec2_key_name = var.ec2_public_key
-  vpc_id = var.vpc_id
-  vpc_mgmt_subnet_ids = var.mgmt_subnets
+
+  mgmt_subnet_security_group_ids = [
+    module.bigiq_mgmt_sg.security_group_id
+  ]
+  private_subnet_security_group_ids = [
+    module.bigiq_sg.security_group_id
+  ]
   vpc_private_subnet_ids = var.private_subnets
+  vpc_mgmt_subnet_ids    = var.mgmt_subnets
+  vpc_id = var.vpc_id
 }
--- AWS/modules/shared/varibles.tf
+++ AWS/modules/shared/varibles.tf
@@ -10,13 +10,18 @@ variable "private_subnets" {
 variable "ec2_public_key" {
   type = string
 }
-variable "licenses" {
-  type = object({
-    cm_key = string
-    dcd_key = string
-  })
+variable "dcd_license_keys" {
+  type = list(string)
 }
-
+variable "cm_license_keys" {
+  type = list(string)
+}
+variable "allowed_mgmt_cidr" {
+  type = string
+}
+variable "ec2_key_name" {
+  type = string
+}
 variable "tags" {
   description = "AWS Tags"
   type = object({
